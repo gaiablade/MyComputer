009316322960689ca31e6e96f42699aebe11c457
gaiablade <cgandeg@gmail.com>
2021-03-17 15:01:16 -0400

    Updated map2

--- Maps/createMap.sh
+++ Maps/createMap.sh
@@ -4,7 +4,7 @@ output_file=`echo ${map_file} | sed "s/\(\w*\)\.tmx/\1.mf/"`
 echo "File: ${output_file}"
 
 # Delete all lines except the layer attribute:
-sed -n "/\(^ <layer.*$\)\|\(^\([0-9]\+,\)\+[0-9],\?$\)/p" $map_file > $output_file
+sed -n "/\(^ <layer.*$\)\|\(^\([0-9]\+,\)\+[0-9]\+,\?$\)/p" $map_file > $output_file
 
 # Extract width and height onto a line:
 sed -i 's/^.*width=\"\([0-9]\+\)\".*height=\"\([0-9]\+\)\".*$/\1, \2/' $output_file
